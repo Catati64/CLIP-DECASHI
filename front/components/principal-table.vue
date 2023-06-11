f4ac7dba94cfde49790b014605e83f6c1a2d13bc
Catati64 <Catati64@hotmail.com>
2023-06-11 15:38:56 -0600

    POP UP - MEDIO FUNCIONAL

--- front/components/principal-table.vue
+++ front/components/principal-table.vue
@@ -41,9 +41,50 @@
         + New Task
       </v-btn>
     </div>
+    <v-dialog v-model="NewTaskDialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Add New Task</span>
+        </v-card-title>
+        <v-card-text>
+          <v-text-field v-model="newTask.description" label="Description" />
+          <v-row>
+            <v-col cols="6">
+              <v-menu offset-y>
+                <template #activator="{ on }">
+                  <v-text-field v-model="newTask.startDate" label="Start Date" readonly v-on="on" />
+                </template>
+                <v-date-picker v-model="newTask.startDate" no-title />
+              </v-menu>
+            </v-col>
+            <v-col cols="6">
+              <v-menu offset-y>
+                <template #activator="{ on }">
+                  <v-text-field v-model="newTask.endDate" label="End Date" readonly v-on="on" />
+                </template>
+                <v-date-picker v-model="newTask.endDate" no-title />
+              </v-menu>
+            </v-col>
+          </v-row>
+          <v-select v-model="newTask.priority" label="Priority" :items="['High', 'Medium', 'Low']" />
+          <v-select v-model="newTask.state" label="State" :items="['Todo', 'In Progress', 'Done']" />
+          <v-text-field v-model="newTask.tags" label="Tags" />
+          <v-textarea v-model="newTask.notes" label="Notes" />
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" text @click="saveTask">
+            Add
+          </v-btn>
+          <v-btn color="secondary" text @click="NewTaskDialog = false">
+            Cancel
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
+<!-- eslint-disable no-console -->
 <script>
 export default {
   data () {
@@ -59,10 +100,22 @@ export default {
         { text: 'Tags', value: 'tags' },
         { text: 'Notes', value: 'notes' }
       ],
+      newTask: {
+        description: '',
+        startDate: null,
+        endDate: null,
+        priority: null,
+        state: null,
+        tags: '',
+        notes: ''
+      },
       items: []
     }
   },
   methods: {
+    saveTask () {
+      console.log('JEJE')
+    },
     getPriorityColor (priority) {
       switch (priority) {
         case 'High':
@@ -129,4 +182,38 @@ export default {
   color: #ffffff;
   font-family: 'Oxanium', sans-serif;
 }
+
+.v-dialog.theme--dark .v-card {
+  background-color: #333333;
+  border-radius: 4px;
+  box-shadow: 0 4px 8px rgba(255, 255, 255, 0.2);
+  padding: 16px;
+}
+
+.v-dialog.theme--dark .v-card-title {
+  color: #ffffff;
+}
+
+.v-dialog.theme--dark .v-card-actions {
+  justify-content: flex-end;
+}
+
+.v-text-field.theme--dark .v-label {
+  color: #ffffff;
+}
+
+.v-text-field.theme--dark .v-input__control,
+.v-text-field.theme--dark .v-input__slot {
+  background-color: #444444;
+  color: #ffffff;
+}
+
+.v-text-field.theme--dark .v-input__control::after {
+  border-bottom-color: #ffffff;
+}
+
+.v-text-field.theme--dark .v-input__control::before {
+  border-bottom-color: #ffffff;
+}
+
 </style>
